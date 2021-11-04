2abf1b32f8e388e423a98dd262e840d1328d6a10
Engineer <sdc320sdc320@gmail.com>
2021-11-04 17:38:25 +0300

    Привет маша

---
+++ Example005_Condition/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("Ура! это же Маша");
+}
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
